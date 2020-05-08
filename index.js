8f665048a36c54092f1142e4850cda9ec301cd93
Luiz Eduardo <luizdudumr@gmail.com>
2020-05-08 19:43:36 -0300

    .gitignore

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const botconfig = require("./botconfig.json");
+const botconfigs = require("./botconfig.json");
 const Discord = require("discord.js");   //Discord API with DiscordJS required
 const bot = new Discord.Client({ disableEveryone: true });     //Bot initiation
 
 const { prefix, token } = require('./botconfig.json');
-bot.login(botconfig.token);
+bot.login(botconfigs.token);
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
@@ -17,7 +17,7 @@ bot.on("message", async message => {
     else if (message.content === `${prefix}hehe`) {
         const usuario = bot.users.cache.get('ID');
         usuario.send('Exemplo');
-    }
+    } //test
 
     else if (message.content === `${prefix}k`) {
         const channel = bot.channels.cache.get('ID Channel');
@@ -26,10 +26,4 @@ bot.on("message", async message => {
 
 
 })
-
-            //const luiz = bot.users.fetch('657770685866967040').catch();
-            //console.log(bot.users.fetch('657770685866967040'));
-            //console.log(bot.users.fetch(657770685866967040, true).username);
-
-            //luiz.send('OI LUIZ KKKKKK');
 
