3ede4c06793e5549c9e046fd91be4596642a02b2
Luiz Eduardo <luizdudumr@gmail.com>
2020-05-09 16:38:49 -0300

    Add username

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const botconfigs = require("./botconfigs.json");
-const Discord = require("discord.js");   //Discord API with DiscordJS required
-const bot = new Discord.Client({ disableEveryone: true });     //Bot initiation
-
-const { prefix, token } = require('./botconfigs.json');
-bot.login(botconfigs.token);
-
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity('As lágrimas do meu desenvolvedor', { type: 'LISTENING' })
-});
-
-bot.on("message", async message => {
-    if (message.content === `${prefix}ping`) {
-        message.channel.send('Pong.');
-    }
-    else if (message.content === `${prefix}hehe`) {
-        const usuario = bot.users.cache.get('ID');
-        usuario.send('Exemplo');
-    } //test
-
-    else if (message.content === `${prefix}k`) {
-        const channel = bot.channels.cache.get('ID Channel');
-        channel.send(':heart:');
-    }
+const Discord = require("discord.js");   //É feita a requisição do Discord.js
+const bot = new Discord.Client({ disableEveryone: true });     //É feita a iniciação do bot através da requisição
+const { prefix, token } = require('./botconfigs.json');  // Variável para usar o prefixo.
 
 
-})
+var inicio = () => {
+    bot.login(`${token}`); // Iniciação do bot
+
+    bot.on("ready", async () => { // Função de inicializaçãod o bot no terminal e mensagem de atividade
+        console.log(`${bot.user.username} is online!`);
+        bot.user.setActivity('As lágrimas do meu desenvolvedor', { type: 'LISTENING' })
+        bot.user.setUsername('Horus Bot');
+        //bot.user.setAvatar('')
+    });
+}
+
+var mensagens = () => {
+    // Função para as mensagens recebidas pelos usuários
+    bot.on("message", async message => {
+        if (message.content === `${prefix}ping`) {
+            message.channel.send('Pong.');
+        }
+        else if (message.content === `${prefix}hehe`) {
+            const usuario = bot.users.cache.get('ID');
+            usuario.send('Exemplo');
+        } //test
+
+        else if (message.content === `${prefix}k`) {
+            const channel = bot.channels.cache.get('ID Channel');
+            channel.send(':heart:');
+        }
+
+
+    })
+}
+
 
